55a3e306a81dd80fb0c9b6d5bd541ba75d2893b2
clover3 <lesterny@gmail.com>
2018-09-23 15:44:38 -0400

    Update read_and_plot.py

--- read_and_plot.py
+++ read_and_plot.py
@@ -5,12 +5,11 @@ import pandas as pd
 from series_cont import get_cont
 import pickle
 
-#x,y = get_cont("facebook")
-#pickle.dump((x,y), open("fb", "wb"))
-x,y = pickle.load(open("fb","rb"))
+x,y = get_cont("facebook")
+pickle.dump((x,y), open("fb", "wb"))
+#x,y = pickle.load(open("fb","rb"))
 stock_data = get_data("FB")
 
-#stock_data['diff'] = stock_data['Close'] - stock_data['Close'].shift(-1)
 stock_data['diff'] = stock_data['Close'].diff().abs()
 stock_data['Controversy'] = pd.Series(y, index=x)
 print(stock_data)
